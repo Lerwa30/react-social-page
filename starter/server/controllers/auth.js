f3c44aba446f251f3d7ac7cd10b5436c51ab3894
Leroy <leroypawlick@gmail.com>
2023-07-11 20:34:59 -0700

    created register function

--- starter/server/controllers/auth.js
+++ starter/server/controllers/auth.js
@@ -1,9 +1,39 @@
+require('dotenv').config();
+
+const {User} = require('../models/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+
+const createToken = (username, id) => {
+    return jwt.sign({username, id}, SECRET, {expiresIn: '2 days'})
+};
+
 module.exports = {
   login: () => {
     console.log("login");
   },
 
-  register: () => {
-    console.log("logout");
-  },
+  register: async (req, res) => {
+    try{
+        const { username, password } = req.body;
+        const foundUser = await User.findOne({where: {username: username}})
+        if (foundUser) {
+            res.status(400).send('user already exists')
+        } else {
+            const salt = bcrypt.genSaltSync(10)
+            const hash = bcrypt.hashSync(password, salt)
+            const newUser = await User.create({username: username, hashedPass: hash})
+            const token = createToken(newUser.dataValues.username, newUser.dataValues.id)
+            console.log(newUser)
+            const exp = Date.now() + 1000 * 60 * 60 * 48
+            res.status(200).send({
+                username: newUser.dataValues.username,
+                userId: newUser.dataValues.id,
+                token,
+                exp})
+        }
+    } catch (error) {
+        console.log(error)
+    }
+  }
 };
